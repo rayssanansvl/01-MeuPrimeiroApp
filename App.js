4e54636534510e6d7a40594e7bc6673f625e1558
rayssanansvl <rayssa.senaiii@gmail.com>
2025-08-26 14:45:45 -0300

    Desenvolvimento do aplicativo

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importar os módulos necessários do React e React Native.
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { StyleSheet, Text, View } from "react-native";
+
+
+
+// Define o componente principal a aplicação "APP"
+// A palavra 'export default' o torna disponível para ser usado em outras partes da aplicação
+export default function App(){
+    //O componente retorna uma estrutura de UI (Interface do Usuário) em 35X
+    return(
+        //'View' é um contêiner flexível, equivalente a uma <div>
+        <View style={styles.container}>
+            {/* 'Text é um componente para a utilização de texto' */}
+            <Text style={styles.title}>Meu primeiro App</Text>
+
+            {/* Outro componente de texto */}
+            <Text style={styles.subtitle}>Bem-vindo ao React-Native!</Text>
+        </View>
+    );
 }
 
+//Cria um objeto de estilo usando a API 'StyleSheet'
+//Isso otimiza o desempenho e facilita a organização dos estilos
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex:1,
+        backgroundColor: "#d9d9d9",
+        alignItems:"center",
+        justifyContent:"center"
+    }
+})
